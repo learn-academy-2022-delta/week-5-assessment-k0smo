36dc229f034aab03639a2b1e0d06d9764a4f6cc7
k0smo <sawallace7@gmail.com>
2022-07-29 08:51:43 -0700

    week 5 please review

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -22,8 +22,39 @@ const secretCodeWord2 = "Gobbledygook"
 const secretCodeWord3 = "Eccentric"
 // Expected output: "3cc3ntr1c"
 
+describe("secretCodeWord", () => {
+    it("should return a coded message", () => {
+        expect(secretCodeWord(secretCodeWord1)).toBe("L4ck4d41s1c4l");
+        expect(secretCodeWord(secretCodeWord2)).toBe("G0bbl3dyg00k");
+        expect(secretCodeWord(secretCodeWord3)).toBe("3cc3ntr1c");
+    })
+})
+
 
 // b) Create the function that makes the test pass.
+
+// Pseudocode:
+// 1. Create a function that takes in a string and returns a coded message.
+// 2. Create a function that takes in an array and a letter and returns an array of words that contain the letter.
+
+
+const secretCodeWord = (word) => {
+    let codedWord = "";
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] === "a") {
+            codedWord += "4";
+        } else if (word[i] === "e") {
+            codedWord += "3";
+        } else if (word[i] === "i") {
+            codedWord += "1";
+        } else if (word[i] === "o") {
+            codedWord += "0";
+        } else {
+            codedWord += word[i];
+        }
+    }
+    return codedWord;
+}
 
 
 // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
@@ -37,8 +68,30 @@ const letterA = "a"
 const letterE = "e"
 // Expected output: ["Cherry", "Blueberry", "Peach"]
 
+describe ("letterInWord", () => {
+    it("should return an array of words containing a letter", () => {
+        expect(letterInWord(fruitArray, letterA)).toEqual(["Mango", "Apricot", "Peach"]);
+        expect(letterInWord(fruitArray, letterE)).toEqual(["Cherry", "Blueberry", "Peach"]);
+    })
+})
+
 
 // b) Create the function that makes the test pass.
+
+// psuedeocode:
+// Create a function that takes in an array of words 
+// and a single letter and returns an array of all the words containing that particular letter.
+// 
+
+const letterInWord = (array, letter) => {
+    let wordsWithLetter = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].includes(letter)) {
+            wordsWithLetter.push(array[i]);
+        }
+    }
+    return wordsWithLetter;
+}
 
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a “full house”. A full house is exactly one pair and one three of a kind.
@@ -52,5 +105,51 @@ const hand2 = [5, 5, 3, 3, 4]
 const hand3 = [5, 5, 5, 5, 4]
 // Expected output: false
 
+describe ("fullHouse", () => {
+    it("should return true if the array is a full house", () => {
+        expect(fullHouse(hand1)).toBe(true);
+        expect(fullHouse(hand2)).toBe(false);
+        expect(fullHouse(hand3)).toBe(false);
+    })
+})
+
 
 // b) Create the function that makes the test pass.
+
+// Psuedocode:
+// 1. Create a function that takes in an array of 5 numbers and determines whether or not the array is a “full house”.
+// 2. Create a function that takes in an array of numbers and returns true if the array is a full house.
+// 3. Create a function that takes in an array of numbers and returns false if the array is not a full house.
+
+const fullHouse = (array) => {
+    let pair = 0;
+    let threeOfaKind = 0;
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if (array[i] === array[j]) {
+                pair++;
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -9,7 +9,16 @@ letter_o = 'o'
 # Expected output: ['coffee', 'soda water']
 letter_t = 't'
 # Expected output: ['tea', 'water', 'soda water']
+# psuedocode:
+# 1. Create a method 
+# 2. Take in an array of words
+# 3. Take in a single letter
+# 4. Return an array of words that contain the letter
 
+
+def letter_check(array, letter)
+  array.select { |word| word.include?(letter) }
+end
 
 # -------------------2) Create a method that takes in an array of numbers and returns the sum of the numbers. Use the test variables provided.
 
@@ -19,11 +28,29 @@ nums_array1 = [42, 7, 27]
 nums_array2 = [25, 17, 47, 11]
 # Expected output: 100
 
+# psuedocode:
+# 1. Create a method
+# 2. Take in an array of numbers
+# 3. Return the sum of the numbers
+
+
+def sum_nums(array)
+  array.reduce(:+)
+end
 
 
 # --------------------3a) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a bike_info method that returns a sentence with all the data from the bike object.
 
 # Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.'
+# Psuedocode:
+# Create a class
+# Initialize with a model, wheels, and current_speed
+# Create a bike_info method that returns a sentence with all the data from the bike object
+
+
+def bike_info(model, wheels, current_speed)
+  "#{model} bike has #{wheels} wheels and is going #{current_speed} mph."
+end
 
 
 
@@ -33,3 +60,14 @@ nums_array2 = [25, 17, 47, 11]
 # Expected output example: my_bike.pedal_faster(18) => 28
 # Expected output example: my_bike.brake(5) => 23
 # Expected output example: my_bike.brake(25) => 0
+
+# Pseudocode:
+
+#  Add the ability to pedal faster and brake.
+#  The pedal_faster method should increase the speed by a given amount.
+#  The brake method should decrease the speed by a given amount.
+#  The bike cannot go negative speeds.
+
+def pedal_faster(current_speed, amount)
+  current_speed + amount
+end
